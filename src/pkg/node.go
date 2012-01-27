24922562a0c032fb53a7da16cb45b75a2b2c41bd
Arne Vansteenkiste <Arne.Vansteenkiste@UGent.be>
2012-01-27 21:25:23 +0100

    add node.go

---
+++ src/pkg/node.go
@@ -0,0 +1,46 @@
+package clip
+
+import(
+	"strings"
+)
+
+// Node in the Library's file tree
+type Node struct{
+	str string
+	parent *Node
+	children []*Node
+}
+
+func NewNode(str string, parent *Node)*Node{
+	node := &Node{str, parent, []*Node{}}
+	if parent != nil{parent.children = append(parent.children, node)}
+	return node
+}
+
+func(n*Node)Add(str string){
+	Debug("Node.Add", str)
+	slash := strings.Index(str, "/")
+	root := str[:slash]
+	Debug("root=", root)
+	base := str[slash+1:]
+	Debug("base=", base)
+	child := n.Child(root)
+	if child == nil{
+		child = NewNode(root, n)
+	}
+}
+
+func(n*Node)String()string{
+	str := n.str
+	for p := n.parent; p!=nil; p=p.parent{
+		str = p.String() + "/" + str
+	}
+	return str
+}
+
+func(n*Node)Child(str string)*Node{
+	for _,c:=range n.children{
+		if c.str == str{return c}
+	}
+	return nil
+}
