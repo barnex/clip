7c1dd7e6c6630d0a5e091b2a2d0384b25d4c9973
Arne Vansteenkiste <Arne.Vansteenkiste@UGent.be>
2012-01-28 22:53:43 +0100

    cleanup

--- src/pkg/client.go
+++ src/pkg/client.go
@@ -7,6 +7,7 @@ import (
 	"exec"
 )
 
+// Main loop for "client" mode (the normal mode).
 func MainClient(args []string) {
 	client := dialDaemon()
 	var resp string
@@ -15,22 +16,33 @@ func MainClient(args []string) {
 	fmt.Println(resp)
 }
 
+// Connect to the clip daemon for RPC communication.
+// Starts the daemon if he's not yet running.
 func dialDaemon() *rpc.Client {
+	// try to call the daemon
 	client, err := rpc.DialHTTP("tcp", "localhost"+PORT)
+
+	// if daemon does not seem to be running, start him.
+	const SLEEP = 10e6 // nanoseconds
 	if err != nil {
 		forkDaemon()
+		time.Sleep(SLEEP)
 	}
+
+	// try again to call the daemon,
+	// give him some time to come up.
 	trials := 0
 	for err != nil && trials < 10 {
 		client, err = rpc.DialHTTP("tcp", "localhost"+PORT)
-		time.Sleep(100e6)
+		time.Sleep(SLEEP)
 		trials++
 	}
+
 	Check(err)
 	return client
 }
 
-
+// Start the clip daemon.
 func forkDaemon() {
 	cmd := exec.Command("clip", "-d")
 	err := cmd.Start()
