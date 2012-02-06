68594d7c73668319a658609aa40c1ef112f3aba4
Arne Vansteenkiste <barnex@gmail.com>
2012-02-06 21:04:31 +0100

    add a few show options

--- src/pkg/show.go
+++ src/pkg/show.go
@@ -4,15 +4,37 @@ package clip
 
 import (
 	"os"
+	"fmt"
+	"strings"
 )
 
 // Register the command
 func init() {
 	player.command["show"] = Show
+	show["playlist"] = showPlaylist
+	show["status"] = showStatus
 }
+
+var show map[string]func()string = make(map[string]func()string)
+
 
 func Show(args []string) (resp string, err os.Error) {
 	// keep map of Showers/Stringers?
 	// also resolve in tags/only if not found?	
 	// show playlist, artists, library, ...
+	object := args[0]
+	f,ok := show[object]
+	if!ok{
+		err = os.NewError("show: not found " + object)
+	}
+	resp = f()
+	return
+}
+
+func showPlaylist() string{
+	return fmt.Sprint(player.playlist)
+}
+
+func showStatus()string{
+	return strings.Replace(fmt.Sprintf("%#v", player), ",", "\n", -1)
 }
