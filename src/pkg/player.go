10c49bf667b1beb90c91c06d769fde966ab97e07
Arne Vansteenkiste <barnex@gmail.com>
2012-02-06 15:57:23 +0100

    rpc.go

--- src/pkg/player.go
+++ src/pkg/player.go
@@ -7,9 +7,6 @@ package clip
 
 import (
 	"os"
-	"rpc"
-	"net"
-	"http"
 )
 
 var player Player = Init()
@@ -64,34 +61,4 @@ func (p *Player) MainDaemon(args []string) {
 			call.respChan <- resp
 		}
 	}
-}
-
-
-// Start serving RPC calls from client instances.
-func (p *Player) serveRPC() {
-	rpc.Register((*PlayerRPC)(&player))
-	rpc.HandleHTTP()
-	conn, err := net.Listen("tcp", p.port)
-	if err != nil {
-		Err("listen error:", err)
-	}
-	Debug("Listening on port " + p.port)
-	http.Serve(conn, nil)
-	//TODO: log errors.
-}
-
-
-// Dummy type to define RPC methods on.
-type PlayerRPC Player
-
-// RPC-exported function used for normal operation mode.
-// The command-line arguments are passed (e.g. "play jazz")
-// and a response to the user is returned in *resp.
-func (rpc *PlayerRPC) Call(args []string, resp *string) (err os.Error) {
-	p := (*Player)(rpc)
-	call := NewCall(args)       // wrap args in Call struct
-	p.callChan <- call          // send to event loop for execution
-	callResp := <-call.respChan // wait for response
-	*resp = callResp.Resp       // set return value
-	return callResp.Err
 }
